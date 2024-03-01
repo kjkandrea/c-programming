40084283214e6e25761434fee22516c14b4cb923
kjkandrea <kjkandrea@gmail.com>
2024-03-01 11:35:02 +0900

    feat: 이름 띄어쓰기 허용

--- 03-01_input-output.c
+++ 03-01_input-output.c
@@ -6,13 +6,13 @@ int main()
     char rightName[16];
 
     printf("이름을 입력하세요: ");
-    scanf("%s", leftName);
+    gets(leftName);
     printf(
         "%s은 영혼과 육체의 이원성에 대해 아무것도 모르고 혐오감이 무엇인지도 모른다\n",
         leftName);
 
     printf("이름을 입력하세요: ");
-    scanf("%s", rightName);
+    gets(rightName);
     printf(
         "그래서 %s는 그의 곁에 있으면 기분이 좋고 편안했던 것이다.\n",
         rightName);
