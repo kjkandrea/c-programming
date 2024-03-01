cf0d5fe58dcb49202927e3ce15b667206174e4d0
kjkandrea <kjkandrea@gmail.com>
2024-03-01 11:18:29 +0900

    feat: 입출력 해보기

---
+++ 03-01_input-output.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    char leftName[8];
+    char rightName[8];
+
+    printf("이름을 입력하세요: ");
+    scanf("%s", leftName);
+    printf(
+        "%s은 영혼과 육체의 이원성에 대해 아무것도 모르고 혐오감이 무엇인지도 모른다\n",
+        leftName);
+
+    printf("이름을 입력하세요: ");
+    scanf("%s", rightName);
+    printf(
+        "그래서 %s는 그의 곁에 있으면 기분이 좋고 편안했던 것이다.\n",
+        rightName);
+
+        return 0;
+};
