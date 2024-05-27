8d49c70020b6bfcf73023035abc8ef218d8e5ca8
kjkandrea <kjkandrea@gmail.com>
2024-05-27 17:59:51 +0900

    feat: 오타 교정 & 코맨트

--- 06-02_pointer-address-print.c
+++ 06-02_pointer-address-print.c
@@ -8,7 +8,7 @@ int main()
     int array[] = {1, 2, 3};
 
     printf("x address : %x\n", &x);
-    printf("array address : %x\n", array);
+    printf("array address : %x\n", array); // 배열의 시작 주소가 출력 됨
     printf("array[0] address : %x\n", &array[0]);
     printf("array[1] address : %x\n", &array[1]);
 }
